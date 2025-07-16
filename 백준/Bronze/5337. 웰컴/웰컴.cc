de4dfc18ae885576d5483b321e2219d927f79e9f
hyojeong0509 <41557022+hyojeong0509@users.noreply.github.com>
2025-07-16 19:33:35 +0900

    [Bronze V] Title: 웰컴, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cout << ".  .   ." << endl;
+    cout << "|  | _ | _. _ ._ _  _" << endl;
+    cout << "|/\\|(/.|(_.(_)[ | )(/." << endl;
+
+    return 0;
+}
