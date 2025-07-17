21f13d62e0b8f5c3196ff40a621766338ea96150
hyojeong0509 <41557022+hyojeong0509@users.noreply.github.com>
2025-07-17 18:56:23 +0900

    [Bronze V] Title: 삼각형, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/29751. 삼각형/삼각형.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int a, b;
+    float c;
+
+    cin >> a >> b;
+    c = (a * b) / 2.0;
+
+    cout << fixed << setprecision(1) << c << endl;
+
+    return 0;
+}
