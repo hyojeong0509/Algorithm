70d434eea40369c84afa7a9d1ac319d391231e9f
hyojeong0509 <41557022+hyojeong0509@users.noreply.github.com>
2025-07-18 22:20:59 +0900

    [Bronze V] Title: Gum Gum for Jay Jay, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/26489. Gum Gum for Jay Jay/Gum Gum for Jay Jay.cc
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    string line;
+    int count = 0;
+
+    while (getline(cin, line))
+    {
+        count++;
+    }
+
+    cout << count << endl;
+
+    return 0;
+}
