20775a8477256d3d4c6426c0ceaf1b43f2717346
hyojeong0509 <41557022+hyojeong0509@users.noreply.github.com>
2025-07-19 19:52:54 +0900

    [Bronze V] Title: 기찍 N, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int num = 0;
+
+    cin >> num;
+
+    for (int i = num; i >= 1; i--)
+    {
+        cout << i << endl;
+    }
+    return 0;
+}
