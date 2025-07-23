fc811354cafe63625b7ffc33851c76c10c643c66
hyojeong0509 <41557022+hyojeong0509@users.noreply.github.com>
2025-07-23 19:23:24 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int num = 0;
+
+    cin >> num;
+
+    cout << num - 543 << endl;
+
+    return 0;
+}
