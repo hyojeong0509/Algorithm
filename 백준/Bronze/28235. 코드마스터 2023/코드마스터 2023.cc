3b216b9b33e3bb02c4bf5c50ad3b43b310f73586
hyojeong0509 <41557022+hyojeong0509@users.noreply.github.com>
2025-07-27 18:20:59 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.cc
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    string s;
+
+    cin >> s;
+
+    if (s == "SONGDO")
+    {
+        cout << "HIGHSCHOOL";
+    }
+    else if (s == "CODE")
+    {
+        cout << "MASTER";
+    }
+    else if (s == "2023")
+    {
+        cout << "0611";
+    }
+    else if (s == "ALGORITHM")
+    {
+        cout << "CONTEST";
+    }
+}
