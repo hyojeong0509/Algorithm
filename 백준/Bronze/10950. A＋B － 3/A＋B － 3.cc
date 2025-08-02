85802fa3ef0985424c17f729e72e8fe281570a37
hyojeong0509 <41557022+hyojeong0509@users.noreply.github.com>
2025-08-02 13:10:39 +0900

    [Bronze V] Title: A+B - 3, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int T;
+    cin >> T;
+
+    while (T--)
+    {
+        int a, b;
+        cin >> a >> b;
+        cout << a + b << endl;
+    }
+
+    return 0;
+}
